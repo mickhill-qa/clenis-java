a47d6e78b01ad82d398f18aeedb8565cd13a5594
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2026-06-24 21:19:27 -0300

    brincando com navegador e teste

--- src/test/java/exemplo/JUnit5Tests.java
+++ src/test/java/exemplo/JUnit5Tests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Funcionalidade: Testes XPTO")
-public class SwitchDeTests {
+public class JUnit5Tests {
 
     @Test
     @DisplayName("Cenario: Valida o nome errado")
